23c7f19bd9af02ddbe5c9fb6b35168500b79fbef
Jose Romeiro <jose.romeiro@elitesoft.com.br>
2021-05-25 20:14:03 -0300

    Fim módulo eloquente

--- behavior/app/Post.php
+++ behavior/app/Post.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace LaraDev;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+
+    public $timestamps = true;
+    //public const CREATED_AT =  'creation_date';
+    //public const UPDATE_AT = 'last_update';
+
+    protected $fillable = ['title', 'subtitle', 'description'];
+    protected $guarded = [];
 }
--- behavior/database/factories/PostFactory.php
+++ behavior/database/factories/PostFactory.php
@@ -2,17 +2,12 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(\LaraDev\Post::class, function (Faker $faker) {
+$factory->define(\App\Post::class, function (Faker $faker) {
 
-    $title = $faker->sentence(10);
     return [
-        'title' => $title,
-        'slug' => str_slug($title),
-        'subtitle' => $faker->sentence(10),
-        'description' => $faker->paragraph(5),
-        'publish_at' => $faker->dateTime(),
-        'category' => function () {
-            return factory(\LaraDev\Categories::class)->create()->id;
-        },
+        'title' => $faker->sentence(8),
+        'subtitle' => $faker->sentence(16),
+        'description' => $faker->paragraph(10)
     ];
+
 });
